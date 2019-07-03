38575010f93e3e643905ec91ba0b1b71f0cffae8
SebastianGaud <sebastiano.gaudeano@live.com>
2019-07-03 18:03:40 +0200

    first implementation

--- series/Series.cs
+++ series/Series.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return numbers.SliceFor(sliceLength).ToArray();
+    }
+
+    public static IEnumerable<string> SliceFor(this string numbers, int sliceLength)
+    {
+        var index = 0;
+        for (int i = 0; i <= numbers.Length; i += sliceLength)
+        {
+            ++index;
+            yield return numbers.Substring(index, i);
+        }
     }
 }
