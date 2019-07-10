f7e42475e0452de84672be06f711545ae5029bb5
SebastianGaud <sebastiano.gaudeano@live.com>
2019-07-10 10:31:54 +0200

    series exercise

--- series/Series.cs
+++ series/Series.cs
@@ -6,16 +6,22 @@ public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        return numbers.SliceFor(sliceLength).ToArray();
+        return ThrowIfNotCompliant(numbers, sliceLength, @else: SliceBy).ToArray();
     }
 
-    public static IEnumerable<string> SliceFor(this string numbers, int sliceLength)
+    public static IEnumerable<string> SliceBy(string numbers, int sliceLength)
     {
-        var index = 0;
-        for (int i = 0; i <= numbers.Length; i += sliceLength)
-        {
-            ++index;
-            yield return numbers.Substring(index, i);
-        }
+        return Enumerable.Range(0, 
+            (numbers.Length - (sliceLength - 1))
+        )
+        .Select(i => {
+            return numbers.Substring(i, sliceLength);
+        });
+    }
+
+    private static IEnumerable<string> ThrowIfNotCompliant(string numbers, int sliceLength, Func<string, int, IEnumerable<string>> @else)
+    {
+        if (numbers.Length < sliceLength || sliceLength < 1)  throw new ArgumentException();
+        return @else(numbers, sliceLength);
     }
 }
