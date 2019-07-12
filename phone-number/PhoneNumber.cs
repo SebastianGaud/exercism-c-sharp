576c43a583446232b0c59ea6eb300dd75cd0b038
SebastianGaud <sebastiano.gaudeano@live.com>
2019-07-12 15:19:46 +0200

    complete phone exercise

--- phone-number/PhoneNumber.cs
+++ phone-number/PhoneNumber.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public class PhoneNumber
 {
     public static string Clean(string phoneNumber)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var phone = new string(phoneNumber.Where(Char.IsDigit).ToArray()).RemoveAreaCodeIfPresent();
+        return phone.IsValidPhoneNumber() ? phone : throw new ArgumentException();
     }
+
+    
+}
+
+
+public static class PhoneNumberExtension
+{
+    public static bool IsValidPhoneNumber(this string phoneNumber) 
+        => Regex.IsMatch(phoneNumber, @"^[2-9][0-9]{2}[2-9][0-9]{6}$");
+
+    public static string RemoveAreaCodeIfPresent(this string phone) 
+        => phone[0] == '1' ? phone.Substring(1, phone.Length - 1) : phone;
 }
