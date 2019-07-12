70f15fa5c25f6b570fc3f28bd2f1a9595f31cefb
SebastianGaud <sebastiano.gaudeano@live.com>
2019-07-12 14:40:21 +0200

    complete exercise

--- difference-of-squares/DifferenceOfSquares.cs
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -1,19 +1,42 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class DifferenceOfSquares
 {
+    private const double Pow = 2;
     public static int CalculateSquareOfSum(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return max.SelectResultFrom(
+            1, 
+            @do: (list) => Math.Pow(list.Sum(), Pow)
+        );  
+        
+        //return (int)Math.Pow(Enumerable.Range(1, max).Sum(), Pow);
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return max.SelectResultFrom(
+            1,
+            @do: (list) => list.Select(x => Math.Pow(x, Pow)).Sum()
+        );
+        
+        //return (int)Enumerable.Range(1, max).Select(x => Math.Pow(x, Pow)).Sum();
     }
 
     public static int CalculateDifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
     }
+}
+
+
+public static class DifferenceOfSquaresExtension 
+{
+    public static int SelectResultFrom(this int to, int from, Func<IEnumerable<int>, double> @do){
+        return (int)@do(Enumerable.Range(from, to));
+    }
+
+
 }
