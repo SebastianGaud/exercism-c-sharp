8a444ef75f73bd3fdd4949ef22be6f5c919a54a0
SebastianGaud <sebastiano.gaudeano@live.com>
2019-07-15 10:40:24 +0200

    complete exercism

--- scrabble-score/ScrabbleScore.cs
+++ scrabble-score/ScrabbleScore.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Linq;
 
 public static class ScrabbleScore
 {
     public static int Score(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return input.ToCharArray().Select(GetPointFromLetter).Sum();
+    }
+
+
+    public static int GetPointFromLetter(char letter)
+    {
+        var l = Char.ToUpper(letter);
+        switch (l)
+        {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'R':
+            case 'S':
+            case 'T':
+                return 1;
+            case 'D':
+            case 'G':
+                return 2;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                return 3;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                return 4;
+            case 'K':
+                return 5;
+            case 'J':
+            case 'X':
+                return 8;
+            case 'Q':
+            case 'Z':
+                return 10;
+            default:
+                return 0;
+        }
     }
 }
