be970d0b0d9ab31e6d43829c3a4702745819f9c1
SebastianGaud <sebastiano.gaudeano@live.com>
2019-07-16 16:15:26 +0200

    finish largest series product exercise

--- largest-series-product/LargestSeriesProduct.cs
+++ largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class LargestSeriesProduct
 {
     public static long GetLargestProduct(string digits, int span) 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // ideally throw exception should be encapsulated in a method who check data
+        var onlyDigits = digits.Where(Char.IsDigit);
+        if (
+            digits.Length != onlyDigits.Count()
+            || digits.Length < span
+            || span < 0
+        )
+        {
+            throw new ArgumentException();
+        }
+
+        return digits.SliceBy(span)
+            .Select(seq => 
+                seq
+                .ToCharArray()
+                .Select(x => Convert.ToInt32(x.ToString()))
+                .Aggregate(1, (num, acc) => num * acc)
+            ).Max();
+    }
+
+
+    // Method already used inside "series" exercise
+    public static IEnumerable<string> SliceBy(this string numbers, int sliceLength)
+    {
+        return Enumerable.Range(0, 
+            (numbers.Length - (sliceLength - 1))
+        )
+        .Select(i => {
+            return numbers.Substring(i, sliceLength);
+        });
     }
 }
