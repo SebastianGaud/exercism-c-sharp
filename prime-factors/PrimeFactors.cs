528d548796204d898ba3fd215e2535c6f7a84ce5
SebastianGaud <sebastiano.gaudeano@live.com>
2019-07-18 17:35:57 +0200

    finish exercise

--- prime-factors/PrimeFactors.cs
+++ prime-factors/PrimeFactors.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class PrimeFactors
 {
     public static int[] Factors(long number)
     {
-        throw new NotImplementedException();
+        return Primes(number).ToArray();
+    }
+
+    public static IEnumerable<int> Primes(long number)
+    {    
+        for (int b = 2; number > 1; b++)
+        {               
+            while (number % b == 0)
+            {
+                number /= b;
+                yield return b;
+            }               
+        }
     }
 }
