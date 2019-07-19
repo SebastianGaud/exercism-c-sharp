4c3287b48595cd5f8ddaa36a462f084da8f4027b
SebastianGaud <sebastiano.gaudeano@live.com>
2019-07-19 16:46:51 +0200

    finish exercise pascal triangle

--- pascals-triangle/PascalsTriangle.cs
+++ pascals-triangle/PascalsTriangle.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 public static class PascalsTriangle
 {
     public static IEnumerable<IEnumerable<int>> Calculate(int rows)
     {
-        throw new NotImplementedException();
+        return Enumerable.Range(1, rows).Select(Rows);
+    }
+        
+    private static IEnumerable<int> Rows(int row)
+    {
+        //just first call for each row
+        yield return 1;
+        var column = 1;
+
+        for (var j = 1; j < row; j++)
+        {
+            column = column * (row - j) / j;
+            yield return column;
+        }
     }
 
 }
