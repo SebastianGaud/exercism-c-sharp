b4c600ba5268ebeb50fd2352609d3b9551f57ac7
SebastianGaud <sebastiano.gaudeano@live.com>
2020-07-24 17:46:43 +0200

    high

--- armstrong-numbers/ArmstrongNumbers.cs
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -7,7 +7,6 @@ public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-
         // get digits from number
         var digits = GetDigitFromNumber(number);
 
@@ -20,36 +19,24 @@ public static class ArmstrongNumbers
         // confrontare la potenza con il numero originale
         bool isSameNumber = number == sum;
         return isSameNumber;
-
     }
-
 
     public static int SumArray(IEnumerable<int> arr)
     {
         return arr.Sum();
     }
 
-
     public static IEnumerable<int> GetDigitFromNumber(int num)
     {
         // estrapolare dal numero le cifre es: n° 153 -> 1 - 5 - 3
         var stringNum = num.ToString();
-
-        // una lista on array dove inserire tutte le cifre
-        foreach (var item in stringNum)
-        {
-            yield return int.Parse(item.ToString());
-        }
+        return stringNum.Select(item => int.Parse(item.ToString()));
     }
 
     public static IEnumerable<int> GetArrayWithDigitsPow(IEnumerable<int> arrNum)
     {
         // un modo per calcolare la potenza
         var pow = arrNum.ToArray().Length;
-
-        foreach (var item in arrNum)
-        {
-            yield return (int)Math.Pow(item, pow);
-        }
+        return arrNum.Select(item => (int)Math.Pow(item, pow));
     }
 }
