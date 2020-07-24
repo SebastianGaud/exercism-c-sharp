73a074eb330f07e174fe1937fc994d829fd2674d
SebastianGaud <sebastiano.gaudeano@live.com>
2020-07-24 17:39:52 +0200

    intermedio-alto

--- armstrong-numbers/ArmstrongNumbers.cs
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -3,27 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 
 
-/*
-
-    An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
-
-    For example:
-
-        9 is an Armstrong number, because 9 = 9^1 = 9
-        10 is not an Armstrong number, because 10 != 1^2 + 0^2 = 1
-        153 is an Armstrong number, because: 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
-        154 is not an Armstrong number, because: 154 != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190
-
-    Write some code to determine whether a number is an Armstrong number.
-
-
-*/
-
-
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
+
         // get digits from number
         var digits = GetDigitFromNumber(number);
 
@@ -36,17 +20,13 @@ public static class ArmstrongNumbers
         // confrontare la potenza con il numero originale
         bool isSameNumber = number == sum;
         return isSameNumber;
+
     }
 
 
     public static int SumArray(IEnumerable<int> arr)
     {
-        int sum = 0;
-        foreach (var item in arr)
-        {
-            sum += item;
-        }
-        return sum;
+        return arr.Sum();
     }
 
 
@@ -56,29 +36,20 @@ public static class ArmstrongNumbers
         var stringNum = num.ToString();
 
         // una lista on array dove inserire tutte le cifre
-        var listNum = new List<int>();
-
         foreach (var item in stringNum)
         {
-            listNum.Add(int.Parse(item.ToString()));
+            yield return int.Parse(item.ToString());
         }
-
-        return listNum;
     }
 
     public static IEnumerable<int> GetArrayWithDigitsPow(IEnumerable<int> arrNum)
     {
-        // immagazzinare all'interno di un array le potenze
-        var list = new List<int>();
-
         // un modo per calcolare la potenza
         var pow = arrNum.ToArray().Length;
 
         foreach (var item in arrNum)
         {
-            list.Add((int)Math.Pow(item, pow));
+            yield return (int)Math.Pow(item, pow);
         }
-
-        return list;
     }
 }
